7de12c01bc8b4de5b5c00ecb5df8bf4f27fb1547
SrinidhI1599 <151204343+SrinidhI1599@users.noreply.github.com>
2026-04-08 21:27:23 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from openai import OpenAI
-from src.graders import grader_episode   # ✅ import your grader
+from graders import grader_episode   # ✅ import your grader
 
 # ---------------- ENV SETUP ---------------- #
 
