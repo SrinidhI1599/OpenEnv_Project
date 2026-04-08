7ddaec349ef741e954cb3a285021abdf9654e2be
SrinidhI1599 <151204343+SrinidhI1599@users.noreply.github.com>
2026-04-08 15:14:37 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -28,3 +28,13 @@ def step(action: Action):
 @app.get("/state")
 def get_state():
     return env.state.dict()
+
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+
+if __name__ == "__main__":
+    main()
